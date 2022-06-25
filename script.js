4baf37081ea747f78ccfbc48a24308425452effa
Oscar Dominguez <dominguez.celada@gmail.com>
2022-06-25 11:07:56 +0200

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Creates an issue on each repo when a certain condition or group of conditions are accomplished
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
